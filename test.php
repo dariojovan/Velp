dc90f1dc84a0c20290f5acff8e0067e17164360c
Amir Sol <amir_sol@outlook.com>
2020-11-13 21:06:17 -0800

    added images

--- test.php
+++ test.php
@@ -1,22 +1,7 @@
 <?php
-$curl = curl_init();
-$movieDbUrl = "https://z4vrpkijmodhwsxzc.stoplight-proxy.io/3/movie/";
-$token = "?api_key=3f36afe25dfd9dadde3e0c9bd458f64b";
-curl_setopt_array($curl, array(
-CURLOPT_URL => $movieDbUrl . "211672" . $token,
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_TIMEOUT => 30,
-CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-CURLOPT_CUSTOMREQUEST => "GET",
-CURLOPT_HTTPHEADER => array(
-"cache-control: no-cache"
-),
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-$response = json_decode($response, true);
-$imgPath = "https://image.tmdb.org/t/p/w500" . $response["poster_path"];
-echo $imgPath;
-curl_close($curl);
+if (_iscurl()){
+    echo "this is enabled"; // will do an action
+}else{
+    echo "this is disabled"; // will do another action
+}
 ?>
