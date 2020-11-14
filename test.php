65172fbf2566c4659421e0ab698a01a00674fe97
Amir Sol <amir_sol@outlook.com>
2020-11-13 21:28:50 -0800

    added images

--- test.php
+++ test.php
@@ -1,3 +1,14 @@
 <?php
-echo 'Curl: ', function_exists('curl_version') ? 'Enabled' : 'Disabled';
+$movieDbUrl = "https://z4vrpkijmodhwsxzc.stoplight-proxy.io/3/movie/";
+$token = "?api_key=3f36afe25dfd9dadde3e0c9bd458f64b";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "moviedb";
+$tmp = '[{"id": 16, "name": "Animation"}, {"id": 35, "name": "Comedy"}, {"id": 10751, "name": "Family"}]';
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+$sql = "SELECT id, genres, original_title FROM moviedb";
+$result = $conn->query($sql);
+var_dump($result);
 ?>
