20d037ee70850e0763cdc96647a934553717c358
Amir Sol <amir_sol@outlook.com>
2020-11-13 19:10:34 -0800

    db read added

--- browse.php
+++ browse.php
@@ -71,7 +71,7 @@
     $username = "root";
     $password = "";
     $dbname = "moviedb";
-    $tmp = "[{'id': 16, 'name': 'Animation'}, {'id': 35, 'name': 'Comedy'}, {'id': 10751, 'name': 'Family'}]";
+    $tmp = '[{"id": 16, "name": "Animation"}, {"id": 35, "name": "Comedy"}, {"id": 10751, "name": "Family"}]';
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     $sql = "SELECT genres, original_title FROM moviedb LIMIT 0,10";
